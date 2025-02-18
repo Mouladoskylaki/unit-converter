73427a4beed1844ecff266095ba4771578cbbc2f
Konstantinos Adamopoulos <adamssorama@gmail.com>
2025-02-18 21:25:54 +0200

    Fix: Improve result formatting for large and small numbers, and fix unit swap for same units in select elements.
    - Added conditional formatting to display large numbers in exponential notation and smaller numbers with locale-based grouping.
    - Fixed issue where unit select elements were not properly swapping when the same unit was selected for both fields.

--- UI.js
+++ UI.js
@@ -9,9 +9,9 @@ const fromUnitSelect = document.getElementById("fromUnitSelect");
 const toUnitSelect = document.getElementById("toUnitSelect");
 const menuButtons = document.querySelectorAll(".conversion-menu button");
 
-export const conversionResultElem = document.getElementById('result-vaule');
+export const conversionResultElem = document.getElementById("result-vaule");
 export let selectedFormulaObj;
-export let formulaDescription = document.getElementById('conversion-operation');
+export let formulaDescription = document.getElementById("conversion-operation");
 
 // Populate select elements
 export const updateSelectOptions = (formulaObject) => {
@@ -25,13 +25,14 @@ export const updateSelectOptions = (formulaObject) => {
     fromUnitSelect.appendChild(fromOption);
 
     const toOption = document.createElement("option");
+    toOption.classList.add("to-option");
     toOption.value = unit;
     toOption.textContent = unit.charAt(0).toUpperCase() + unit.slice(1);
     toUnitSelect.appendChild(toOption);
   });
 };
 
-//Add inpput listeners
+// Add inpput listeners
 export const addInputListeners = (
   fromUnitInput,
   fromUnitSelect,
@@ -43,7 +44,7 @@ export const addInputListeners = (
   toUnitSelect.addEventListener("change", updateConversionResult);
 };
 
-//Add click event listeners to each nav button
+// Add click event listeners to each nav button
 menuButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const selectedType = event.target.dataset.type;
@@ -51,16 +52,16 @@ menuButtons.forEach((button) => {
     if (selectedType === "temperature") {
       updateSelectOptions(temperatureFormulas);
       selectedFormulaObj = temperatureFormulas;
+      fromUnitSelect.value = "celsius";
+      toUnitSelect.value = "fahrenheit";
       fromUnitInput.value = 1;
-      fromUnitSelect.value = 'celsius';
-      toUnitSelect.value = 'fahrenheit';
       updateConversionResult();
     } else if (selectedType === "length") {
       updateSelectOptions(lengthFormulas);
       selectedFormulaObj = lengthFormulas;
+      fromUnitSelect.value = "meters";
+      toUnitSelect.value = "centimeters";
       fromUnitInput.value = 1;
-      fromUnitSelect.value = 'meters';
-      toUnitSelect.value = 'centimeters';
       updateConversionResult();
     }
 
@@ -68,63 +69,111 @@ menuButtons.forEach((button) => {
   });
 });
 
-//On load conversion
+// On load conversion
 const initializeConversion = () => {
   updateSelectOptions(lengthFormulas);
   selectedFormulaObj = lengthFormulas;
-  fromUnitSelect.value = 'meters';
-  toUnitSelect.value = 'centimeters';
+  fromUnitSelect.value = "meters";
+  toUnitSelect.value = "centimeters";
   fromUnitInput.value = 1;
-  formulaDescription.innerText = lengthFormulas[fromUnitSelect.value][toUnitSelect.value].description;
+  formulaDescription.innerText =
+    lengthFormulas[fromUnitSelect.value][toUnitSelect.value].description;
   updateConversionResult();
 };
 
-document.addEventListener('DOMContentLoaded', initializeConversion);
+document.addEventListener("DOMContentLoaded", initializeConversion);
 
-//Select starting input
-fromUnitInput.addEventListener('focus', (event) => {
+// Select starting input
+fromUnitInput.addEventListener("focus", (event) => {
   event.target.select();
 });
 
-//Check and update if formula description is available
+// Check and update if formula description is available
 export const updateFormulaDescription = () => {
-  const formula = selectedFormulaObj[fromUnitSelect.value] && selectedFormulaObj[fromUnitSelect.value][toUnitSelect.value];
-  const description = formula && formula.description ? formula.description : "Formula description not available.";
+  const formula =
+    selectedFormulaObj[fromUnitSelect.value] &&
+    selectedFormulaObj[fromUnitSelect.value][toUnitSelect.value];
+  const description =
+    formula && formula.description
+      ? formula.description
+      : "No conversion required.";
   formulaDescription.innerText = description;
 };
 
-//Swap units if they are the same
-function handleFocus(event) {
-  let select = event.target;
-  let otherSelect = select === fromUnitSelect ? toUnitSelect : fromUnitSelect;
-  let temp = select.value;
-
-  function handleChange() {
-    if (select.value === otherSelect.value) {
-      otherSelect.value = temp;
-      updateConversionResult();
-    }
-    select.removeEventListener("change", handleChange);
-  }
-
-  select.addEventListener("change", handleChange);
-}
-
-[fromUnitSelect, toUnitSelect].forEach((select) => {
-  select.addEventListener('focus', handleFocus);
-});
-
-//Update conversion-result element
+// Update conversion-result element
 export const updateConversionResultElem = () => {
   let fromUnitSelectOption = fromUnitSelect.value;
   let toUnitSelectOption = toUnitSelect.value;
-  conversionResultElem.innerText = 
-  `${fromUnitInput.value} ${fromUnitSelectOption.charAt(0).toUpperCase() + fromUnitSelectOption.slice(1)} = ${toUnitInput.value} ${toUnitSelectOption.charAt(0).toUpperCase() + toUnitSelectOption.slice(1)}`;
+  conversionResultElem.innerText = `${fromUnitInput.value} ${
+    fromUnitSelectOption.charAt(0).toUpperCase() + fromUnitSelectOption.slice(1)
+  } = ${toUnitInput.value} ${
+    toUnitSelectOption.charAt(0).toUpperCase() + toUnitSelectOption.slice(1)
+  }`;
 };
 
+// export let fromTemp;
+// export let toTemp;
+// //
+// fromUnitSelect.addEventListener('focus', (event) => {
+//   if (fromUnitSelect.value === toUnitSelect.value) {
+//     console.log('Focus same');
+//     return;
+//   } else {
+//   fromTemp = fromUnitSelect.value;
+//   toTemp = toUnitSelect.value;
+//   console.log("-Focus");
+//   console.log("fromTemp:", fromTemp);
+//   console.log("toTemp:", toTemp);
+//   console.log("fromUnitSelect.value:", fromUnitSelect.value);
+//   console.log("toUnitSelect.value:", toUnitSelect.value);
+//   console.log('/focus');
+//   }
+// });
 
+// fromUnitSelect.addEventListener('change', () => {
+//   if (fromUnitSelect.value === toUnitSelect.value) {
+//     console.log('Change same');
+//     return;
+//   } else {
+//   fromTemp = fromUnitSelect.value;
+//   console.log("-Change");
+//   console.log("fromTemp:", fromTemp);
+//   console.log("toTemp:", toTemp);
+//   console.log("fromUnitSelect.value:", fromUnitSelect.value);
+//   console.log("toUnitSelect.value:", toUnitSelect.value);
+//   console.log("/Change");
+//   }
+// });
+// //
+// toUnitSelect.addEventListener('focus', (event) => {
+//   if (toUnitSelect.value === fromUnitSelect.value) {
+//     console.log('Focus same');
+//     return;
+//   } else {
+//   fromTemp = fromUnitSelect.value;
+//   toTemp = toUnitSelect.value;
+//   console.log("-Focus");
+//   console.log("fromTemp:", fromTemp);
+//   console.log("toTemp:", toTemp);
+//   console.log("toUnitSelect.value:", toUnitSelect.value);
+//   console.log("fromUnitSelect.value:", fromUnitSelect.value);
+//   console.log('/focus');
+//   }
+// });
 
-
-
+// toUnitSelect.addEventListener('change', () => {
+//   if (toUnitSelect.value === fromUnitSelect.value) {
+//     console.log('Change same');
+//     return;
+//   } else {
+//   toTemp = toUnitSelect.value;
+//   console.log("-Change");
+//   console.log("fromTemp:", fromTemp);
+//   console.log("toTemp:", toTemp);
+//   console.log("toUnitSelect.value:", toUnitSelect.value);
+//   console.log("fromUnitSelect.value:", fromUnitSelect.value);
+//   console.log("/Change");
+//   }
+// });
 
 
--- index.js
+++ index.js
@@ -1,30 +1,55 @@
+// index.js
 import { addInputListeners } from "./UI.js";
 import { selectedFormulaObj } from "./UI.js";
 import { updateFormulaDescription } from "./UI.js";
 import { updateConversionResultElem } from "./UI.js";
+import { conversionResultElem } from "./UI.js";
 
 const fromUnitInput = document.getElementById("fromUnitInput");
 const toUnitInput = document.getElementById("toUnitInput");
 const fromUnitSelect = document.getElementById("fromUnitSelect");
 const toUnitSelect = document.getElementById("toUnitSelect");
+let fromTemp;
+let toTemp;
+
+// Update select temp values
+function updateTempValues() {
+  if (fromUnitSelect.value !== toUnitSelect.value) {
+    fromTemp = fromUnitSelect.value;
+    toTemp = toUnitSelect.value;
+  }
+}
+
+fromUnitSelect.addEventListener("focus", updateTempValues);
+fromUnitSelect.addEventListener("change", () => {
+  if (fromUnitSelect.value !== toUnitSelect.value) {
+    fromTemp = fromUnitSelect.value;
+  }
+});
+
+toUnitSelect.addEventListener("focus", updateTempValues);
+toUnitSelect.addEventListener("change", () => {
+  if (toUnitSelect.value !== fromUnitSelect.value) {
+    toTemp = toUnitSelect.value;
+  }
+});
 
 //Convert a value from one unit to another
 function convertUnits(formulaObject, fromUnit, toUnit, value) {
   if (formulaObject[fromUnit] && formulaObject[fromUnit][toUnit]) {
     return formulaObject[fromUnit][toUnit].formula(value);
   } else {
-    throw new Error(`Conversion from ${fromUnit} to ${toUnit} not supported.`);
+    console.warn(`Conversion from ${fromUnit} to ${toUnit} not supported.`);
+    return value
   }
 }
-
 //Update conversion result
 export function updateConversionResult() {
   const fromValue = parseFloat(fromUnitInput.value);
-
   if (fromUnitInput.value.trim() === "") {
     toUnitInput.value = "";
+    conversionResultElem.textContent = "";
     updateFormulaDescription();
-    console.log('empty')
     return;
   }
 
@@ -34,7 +59,13 @@ export function updateConversionResult() {
   }
 
   if (fromUnitSelect.value === toUnitSelect.value) {
-    console.warn("Same unit detected, swapping units.");
+    console.warn("fromUnitSelect - Same unit detected.");
+    toUnitSelect.value = fromTemp;
+    fromUnitSelect.value = toTemp;
+    fromTemp = fromUnitSelect.value;
+    toTemp = toUnitSelect.value;
+    updateFormulaDescription();
+    updateConversionResultElem();
     return;
   }
 
@@ -43,7 +74,7 @@ export function updateConversionResult() {
     toUnitInput.value = "";
     return;
   }
-
+  
   const result = convertUnits(
     selectedFormulaObj,
     fromUnitSelect.value,
@@ -51,7 +82,9 @@ export function updateConversionResult() {
     fromValue
   );
 
-  const formattedResult = parseFloat(result.toPrecision(6));
+  const formattedResult =
+  result >= 1e9 || result <= -1e9 ? result.toExponential(5) : result.toLocaleString();
+  
   toUnitInput.value = formattedResult;
   updateFormulaDescription();
   updateConversionResultElem();
@@ -66,7 +99,11 @@ addInputListeners(
 
 //////////////////////////////////////////////////////////
 const btn = document.querySelector(".submit-test-button");
-btn.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log(selectedFormulaObj);
+btn.addEventListener("click", () => {
+    console.log("-button");
+    console.log("fromTemp:", fromTemp);
+    console.log("toTemp:", toTemp);
+    console.log("fromUnitSelect.value:", fromUnitSelect.value);
+    console.log("toUnitSelect.value:", toUnitSelect.value);
+    console.log("/button");
 });
